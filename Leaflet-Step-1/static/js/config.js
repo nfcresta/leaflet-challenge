734e18a4d37fca4e2704b013aea1c034d8be7ff2
nfcresta <72232054+nfcresta@users.noreply.github.com>
2021-03-10 11:02:14 -0500

    initial work

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoibmZjcmVzdGEiLCJhIjoiY2tsc25pbWN0MXluNDJ2a2RvOWhvNzJtYiJ9.a-_HNj0waMk9lGvW1Tz64w";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,34 @@
+// Create our initial map object
+// Set the longitude, latitude, and the starting zoom level
+var myMap = L.map("mapid").setView([39.8283, -98.5795], 5);
+
+// add tile layer to map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// create function to enlarge markers
+function markerSize(marker) {
+    return marker * 100
+}
+
+// create circles
+function createCircles(response) {
+    var locations = response.features;
+
+    for (var i=0; i < locations.length; i++) {
+        L.circle([locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0]], {
+            color: "black",
+            fillColor: "green",
+            radius: markerSize(locations[i].properties.mag)
+        }).bindPopup(`<strong>${locations[i].properties.place}</strong><hr>Magnitude: ${locations[i].properties.mag}`)
+          .addTo(myMap);
+    }
+}
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", createCircles);
