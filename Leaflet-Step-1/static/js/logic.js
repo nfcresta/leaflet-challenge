ee757d9bef3399f318789f677b55cd93d1a8eef4
nfcresta <72232054+nfcresta@users.noreply.github.com>
2021-03-10 18:41:05 -0500

    almost done with pt1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,9 @@
 // Create our initial map object
 // Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("mapid", {
+var myMap = L.map("map-id", {
     center: [39.8283, -98.5795],
-    zoom: 5
-    });
+    zoom: 4
+});
 
 // add tile layer to map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -16,7 +16,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // create circles
-var dataLink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+var dataLink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
 
 d3.json(dataLink, function(response) {
     console.log(response);
@@ -24,33 +24,39 @@ d3.json(dataLink, function(response) {
 
     // create function to enlarge markers
     function markerSize(marker) {
-        return marker * 100;
+        return marker * 20000;
     }
 
     // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
-    function chooseColor(magnitude) {
+    function chooseColor(depth) {
         switch (true) {
-        case magnitude > 5:
-            return "#581845";
-        case magnitude > 4:
-            return "#900C3F";
-        case magnitude > 3:
-            return "#C70039";
-        case magnitude > 2:
-            return "#FF5733";
-        case magnitude > 1:
-            return "#FFC300";
+        case depth > 50:
+            return "black";
+        case depth > 40:
+            return "purle";
+        case depth > 20:
+            return "maroon";
+        case depth > 10:
+            return "red";
+        case depth > 1:
+            return "orange";
         default:
-            return "#DAF7A6";
+            return "yellow";
         }
     }
 
     for (var i=0; i < locations.length; i++) {
         L.circle([locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0]], {
-            color: "black",
-            fillColor: chooseColor(locations[i].properties.mag),
-            radius: markerSize(locations[i].geometry.coordinates[2])
-        }).bindPopup(`<strong>${locations[i].properties.place}</strong><hr>Magnitude: ${locations[i].properties.mag}`)
+            color: "white",
+            fillColor: chooseColor(locations[i].geometry.coordinates[2]),
+            radius: markerSize(locations[i].properties.mag),
+            fillOpacity: 0.75
+        }).bindPopup(`<strong>${locations[i].properties.place}</strong><hr>Magnitude: ${locations[i].properties.mag}<br>Depth: ${locations[i].geometry.coordinates[2]}`)
           .addTo(myMap);
     }
 });
+
+// set up the legend
+var legend = L.control({ position: "bottomleft" });
+
+
