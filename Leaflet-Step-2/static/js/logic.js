dfc2319efbcd8c6f318a1728a30bbb72c9e300aa
nfcresta <72232054+nfcresta@users.noreply.github.com>
2021-03-12 11:25:30 -0500

    working on pt2

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -2,59 +2,11 @@
 var dataLink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
 var platesLink = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Initialize & Create Two Separate LayerGroups: earthquakes & tectonicPlates
-var earthquakes = new L.LayerGroup();
-var tectonicplates = new L.LayerGroup();
 
-// createMap function
-function createMap(earthquakes, tectonicplates) {
-    // Define Variables for Tile Layers
-    var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.satellite",
-        accessToken: API_KEY
-    });
+// add earthquake circles to map
+var earthquakeMarkers = [];
 
-    var grayscaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.light",
-        accessToken: API_KEY
-    });
-
-    var outdoorsMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.outdoors",
-        accessToken: API_KEY
-    });
-
-    // Define baseMaps Object to Hold Base Layers
-    var baseMaps = {
-        "Satellite Map": satelliteMap,
-        "Grayscale Map": grayscaleMap,
-        "Outdoors Map": outdoorsMap
-    };
-
-    // Create Overlay Object to Hold Overlay Layers
-    var overLayMaps = {
-        "Earthquakes": earthquakes,
-        "Tectonic Planes": tectonicplates
-    };
-
-    // create map object with options
-    var myMap = L.map("map-id", {
-        center: [39.8283, -98.5795],
-        zoom: 4,
-        layers: [satelliteMap, earthquakes]
-    });
-
-    // create layer control
-    L.control.layers(baseMaps, overLayMaps).addTo(myMap);
-}
-
-function createEarthquakeMarkers(response) {
+d3.json(dataLink, function(response) {  
     console.log(response);
     
     var locations = response.features;
@@ -82,8 +34,6 @@ function createEarthquakeMarkers(response) {
         }
     }
     
-    var earthquakeMarkers = [];
-    
     for (var i=0; i < locations.length; i++) {
         earthquakeMarkers.push(L.circle([locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0]], {
             color: "white",
@@ -93,21 +43,68 @@ function createEarthquakeMarkers(response) {
         }).bindPopup(`<strong>${locations[i].properties.place}</strong><hr>Magnitude: ${locations[i].properties.mag}<br>Depth: ${locations[i].geometry.coordinates[2]}`)
         );
     }
+});
+console.log(earthquakeMarkers);
 
-    // create a lyer group made from earthquakes, pass it in create map function
-    var earthquakeMarkerLayer = L.layerGroup(earthquakeMarkers);
-    createMap(earthquakeMarkerLayer);
-}
+// add tectonic plates lines to map
+var tectonicPlateLine = [];
 
-function createTectonicPlateMarkers(response) {
-    var tectonicPlateLine = [];
+d3.json(platesLink, function(response) { 
     tectonicPlateLine.push(L.geoJson(response, {
         color: "#DC143C",
         weight: 2
     }))
-    var tectonicPlateLineLayer = L.layerGroup(tectonicPlateLine);
-    tectonicPlateLineLayer.addTo(myMap);
-}
+});
+console.log(tectonicPlateLine);
 
-d3.json(dataLink, createEarthquakeMarkers);
-d3.json(platesLink, createTectonicPlateMarkers);
+// create base layers
+var earthQuakeMarkersLayer = L.layerGroup(earthquakeMarkers);
+var tectonicPlateLineLayer = L.layerGroup(tectonicPlateLine);
+
+// Define Variables for Tile Layers
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+});
+
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+});
+
+var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+});
+
+// Define baseMaps Object to Hold Base Layers
+var baseMaps = {
+    "Satellite Map": satelliteMap,
+    "Light Map": lightMap,
+    "Dark Map": darkMap
+};
+
+// Create Overlay Object to Hold Overlay Layers
+var overLayMaps = {
+    "Earthquakes": earthQuakeMarkersLayer,
+    "Tectonic Planes": tectonicPlateLineLayer
+};
+
+// create map object with options
+var myMap = L.map("map-id", {
+    center: [39.8283, -98.5795],
+    zoom: 4,
+    layers: [satelliteMap, earthQuakeMarkersLayer]
+});
+
+// create layer control
+L.control.layers(baseMaps, overLayMaps).addTo(myMap);
+
+
+
